abecfd80cff329a366adea4ff1a18e68e12cd152
Sean Aubin <saubin@uwaterloo.ca>
2016-02-09 16:20:45 -0500

    done!

--- Assignment1/debug.py
+++ Assignment1/debug.py
@@ -17,21 +17,42 @@ def trans_cov(dat, cov, mean=np.array([[0],[0]])):
     
     return res
 
-def map_class(c1, c2):
-    e1 = np.cov(c1)
-    e2 = np.cov(c2)
-    u1 = np.mean(c1, axis=1)
-    u2 = np.mean(c2, axis=1)
-    
-    def f(x):
-        #ipdb.set_trace()
-        return np.log(np.sqrt(norm(e2))/np.sqrt(norm(e1))) \
-               -0.5*np.dot(np.dot((x - u1),inv(e1)),(x - u1).T) \
-               +0.5*np.dot(np.dot((x - u2),inv(e2)),(x - u2).T) \
-                < 0
-    return f
+def knn(c1, c2, k):
+    # initiaslise with K points from each class
+    zero_shape = (c1.shape[0], c1.shape[1]*2)
+    c1_res = np.zeros(zero_shape)
+    c1_count = 0
+    c2_res = np.zeros(zero_shape)
+    c2_count = 0
+    c_all = np.concatenate((c1, c2), axis=1)
 
-"""
+    # because numpy nditer is psychotic
+    for c_ind in xrange(c_all.shape[1]):
+        val = c_all[:, c_ind]
+        #find the nearest K neighbours
+        if np.allclose(val, np.array([-2, 0])):
+            ipdb.set_trace()
+        ind = np.argpartition(norm(c_all.T - val, axis=1), k+1)[:k+1][1:k+1]
+        
+        # class the point where the majority of the neighbours are
+        sort_res = 0
+        for ix in ind:
+            if ix < c1.shape[1]:
+                sort_res += 1
+            else:
+                sort_res -= 1
+
+        if sort_res > 0:
+            c1_res[:, c1_count] = val
+            c1_count += 1
+        else:
+            c2_res[:, c2_count] = val
+            c2_count += 1
+
+    assert c1_count + c2_count == c1.shape[1] + c1.shape[1]
+    return (c1_res[:, :c1_count], c2_res[:, :c2_count])
+
+
 dats = [
             [trans_cov(np.random.randn(2, 200), np.eye(2)),
              trans_cov(np.random.randn(2, 200), np.eye(2), np.array([[3],[0]]))],
@@ -43,49 +64,25 @@ dats = [
              trans_cov(np.random.randn(2, 200), np.array([[7,-3],[-3,4]]), np.array([[3],[0]]))]
         ]
 
-
+"""
 dat = [
     np.array([[-2, 0], [-2, 1], [-2,-1], [3, 0]]).T,
     np.array([[2, 0], [2, 1], [2,-1], [-3, 0]]).T
 ]
 """
-"""
-# red left, blue right
-fig = plt.figure()
-plt.scatter(dat[0][0], dat[0][1], color="red")
-plt.scatter(dat[1][0], dat[1][1], color="blue")
-plt.show()
-"""
-
-dat = [scipy.io.loadmat("case4.mat")["a"].T, scipy.io.loadmat("case4.mat")["b"].T]
-map_func = map_class(dat[0], dat[1])
-dat_list = list(np.concatenate((dat[0], dat[1]), axis=1).T)
-
-res = []
-for d_l in dat_list:
-    res.append(map_func(d_l))
-
-
-# grid the whole space
-all_dat = np.array(dat_list)
-a_res = np.array(res, dtype=np.int)
-a_res[a_res == 0] = -1
-
+dat = dats[2]
+all_dat = np.concatenate((dat[0], dat[1]), axis=1).T
 min_x = np.min(all_dat[:, 0])
 max_x = np.max(all_dat[:, 0])
 min_y = np.min(all_dat[:, 1])
 max_y = np.max(all_dat[:, 1])
-delta = 0.01 # make the delta proportional to the size of stuff
-# Draw seperation lines and don't classify the points
-x = np.arange(min_x, max_x, delta)
-y = np.arange(min_y, max_x, delta)
-"""
+sample_points = 1000
+x = np.linspace(min_x, max_x, sample_points)
+y = np.linspace(min_y, max_y, sample_points)
 grid_x, grid_y = np.meshgrid(x, y)
-#grid_x, grid_y = np.mgrid[min_x:max_x:500j, min_y:max_y:500j]
-grid_res = griddata(all_dat, a_res, (grid_x, grid_y), method='cubic')
-plt.contour(grid_x.T, grid_y.T, grid_res.T, levels=[0])
-plt.show()
-"""
 
-# poke values in the correct places
-#ipdb.set_trace()
+res1, res2 = knn(dat[0], dat[1], 1)
+a_res = np.concatenate((np.ones(res1.shape[1]), np.ones(res2.shape[1])*-1))
+grid_res = griddata(all_dat, a_res, (grid_x, grid_y), method='nearest')
+print(grid_res.shape)
+plt.contour(grid_x.T, grid_y.T, grid_res.T, levels=[0], colors='blue')
